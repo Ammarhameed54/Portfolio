0ac0f2a17cb324cb249011ff0c458d5da5350dd8
ammar <f21bdats1m02054@iub.edu.pk>
2023-10-27 09:13:21 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Youtuber", "Web Developer"] ,
+    typespeed: 100,
+    backspeed: 100,
+    backdelay:1000,
+    loop: true
+})
